616d416704cd385fb8542090cff44b9cc8623cbc
MadPsyentist <lachie.fin@gmail.com>
2016-01-18 00:14:56 +1300

    CollisionSystem implemented

--- core/src/com/madpsyence/galaxyinsurgents/Components/BoundsComponent.java
+++ core/src/com/madpsyence/galaxyinsurgents/Components/BoundsComponent.java
@@ -2,6 +2,7 @@ package com.madpsyence.galaxyinsurgents.Components;
 
 import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.math.Rectangle;
+import com.madpsyence.galaxyinsurgents.Entities.EntityType;
 
 /**
  * Created by lachi on 11/01/2016.
@@ -9,9 +10,11 @@ import com.badlogic.gdx.math.Rectangle;
 public class BoundsComponent implements Component
 {
     public Rectangle Bound;
+    public EntityType Type;
 
-    public BoundsComponent(Rectangle Bound)
+    public BoundsComponent(Rectangle Bound, EntityType Type)
     {
         this.Bound = Bound;
+        this.Type = Type;
     }
 }
---
+++ core/src/com/madpsyence/galaxyinsurgents/Systems/CollisionSystem.java
@@ -0,0 +1,54 @@
+package com.madpsyence.galaxyinsurgents.Systems;
+
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.EntitySystem;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.signals.Signal;
+import com.badlogic.ashley.utils.ImmutableArray;
+import com.madpsyence.galaxyinsurgents.Components.BoundsComponent;
+import com.madpsyence.galaxyinsurgents.Events.CollisionEvent;
+
+import java.util.Map;
+
+/**
+ * Created by Lachie on 17/1/2016.
+ */
+public class CollisionSystem extends EntitySystem
+{
+    private Signal<CollisionEvent> CollisionEventSignal;
+
+    private ComponentMapper<BoundsComponent> boundsMap;
+
+    private ImmutableArray<Entity> entities;
+
+    public CollisionSystem(Signal<CollisionEvent> CollisionEventSignal)
+    {
+        this.CollisionEventSignal = CollisionEventSignal;
+    }
+
+    public void addedToEngine(Engine engine)
+    {
+        entities = engine.getEntitiesFor(Family.all(BoundsComponent.class).get());
+        boundsMap = ComponentMapper.getFor(BoundsComponent.class);
+    }
+
+    public void update(float deltaTime)
+    {
+        for(int i = 0; i < (entities.size() - 1); i++)
+        {
+            Entity entityA = entities.get(i);
+            BoundsComponent BA = boundsMap.get(entityA);
+            for(int j = i+1; j < entities.size(); j++)
+            {
+                Entity entityB = entities.get(j);
+                BoundsComponent BB = boundsMap.get(entityB);
+
+                if(BA.Bound.overlaps(BB.Bound))
+                    CollisionEventSignal.dispatch(new CollisionEvent(entityA, entityB));
+            }
+        }
+
+    }
+}
